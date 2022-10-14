14ed77771c519cc5dfa398fc5a75f9eb5b12b626
tung <tung@example.com>
2022-10-14 15:51:15 +0700

    aaa

--- src/test.java
+++ src/test.java
@@ -1,10 +1,31 @@
-import java.io.File;
 
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author TVD
+ */
 public class test {
 
     public static void main(String[] args) {
-        File file = new File("C:\\Developer\\BaiTap_Javacore_1\\inPut1.txt");
-        File[] array = file.listFiles();
-        System.out.println(array == null);
+        Date date = new Date();
+        DateFormat dateFormat;
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println("dd/MM/yyyy: " + dateFormat.format(date));
+        dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        System.out.println("MM/dd/yyyy: " + dateFormat.format(date));
+        dateFormat = new SimpleDateFormat("hh:mm:ss MM/dd/yyyy");
+        System.out.println("hh:mm:ss MM/dd/yyyy: " + dateFormat.format(date));
+        dateFormat = new SimpleDateFormat("HH:mm:ss MM/dd/yyyy");
+        System.out.println("HH:mm:ss MM/dd/yyyy: " + dateFormat.format(date));
+        dateFormat = new SimpleDateFormat("HH:mm:ss.SSS MM/dd/yyyy");
+        System.out.println("HH:mm:ss.SSS MM/dd/yyyy: " + dateFormat.format(date));
+        dateFormat = new SimpleDateFormat("HH:mm:ss");
+        System.out.println("HH:mm:ss: " + dateFormat.format(date));
+
     }
+
 }
--- src/vpbank/baitap/BaiTap_Day9/Main.java
+++ src/vpbank/baitap/BaiTap_Day9/Main.java
@@ -5,6 +5,7 @@ import vpbank.baitap.BaiTap_Day9.Excecutor.Bai9;
 public class Main {
     public static void main(String[] args) {
         try {
+
             Bai9 obj = new Bai9();
             obj.addFileInput("C:\\Developer\\BaiTap_Javacore_1\\BaiTap_Day9\\input");
             obj.readFiles();
